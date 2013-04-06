516907a377e79db9e243b74ea77df7a8bb2d1a57
Delmo <delmosaurio@gmail.com>
2013-04-05 21:14:49 -0300

    - change the module expose

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-#!/usr/bin/env node
 
-var app = require('./lib/scriptme');
-
-var static_dir = '.';
-
-if (process.argv.length > 2) {
-    static_dir = process.argv[2];
-}
-
-app.init(static_dir);
+module.exports = require('./lib/scriptme');
