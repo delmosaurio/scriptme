55324f27e14b2ea46e2246c99a891dd34a1dcb95
Delmo <delmosaurio@gmail.com>
2013-04-10 23:49:06 -0300

    some testing here

--- test/run.js
+++ test/run.js
@@ -1,24 +1,37 @@
-// test for scriptme
+  process.env.NODE_ENV = 'test';
 
-var 
-    // ext modules  
-    fs = require('fs'),
-    basename = require('path').basename,
-    dirname = require('path').dirname,
-    resolve = require('path').resolve,
-    join = require('path').join,
-    mkdirp = require('mkdirp')
-    // scriptme modules
-    scriptme = require('../lib/scriptme');
+  var fs = require('fs')
+    , basename = require('path').basename
+    , dirname = require('path').dirname
+    , resolve = require('path').resolve
+    , join = require('path').join
+    , mkdirp = require('mkdirp')
+    , scriptme = require('../lib/scriptme')
+    , assert = require('assert');;
 
-var context = { description: 'my jQuery plugin description' };
+describe('Rendering the template `jquery.plugin.js`', function() {
 
-var template = fs.readFileSync('./test/cases/jquery.plugin.js', 'utf8');
+    before(function(done){
+        mkdirp('cases/output', function(err){
+            
+            if (err) { throw err; }
 
-mkdirp('./test/cases/output');
+            var context = { description: 'my jQuery plugin description' };
+            var template = fs.readFileSync('./test/cases/jquery.plugin.js', 'utf8');
+            var writer = fs.createWriteStream('./test/cases/output/myplugin.js');
 
-var writer = fs.createWriteStream('./test/cases/output/myplugin.js');
+            scriptme.render(template, writer, context, function(err, response) {
+               if (err) { throw err; }
 
-scriptme.render(template, writer, context, function(err, response) {
-   if (err) console.error(err);
+               done();
+            });
+
+
+        }); 
+    });
+
+    it('the myplugin.js created', function(){
+        assert.ok(fs.existsSync('./test/cases/output/myplugin.js'));
+    });
+
 });
