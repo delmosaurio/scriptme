3ba0588ece769fd53b43e059d9146a398217158e
Delmo <delmosaurio@gmail.com>
2013-10-17 00:11:08 -0300

    implement commander

--- make.bat
+++ make.bat
@@ -4,6 +4,7 @@ set REPORTER=dot
 
 if "%1"=="" goto run
 if "%1"=="run" goto run
+if "%1"=="test-bin" goto test-bin
 if "%1"=="test" goto test
 if "%1"=="test-w" goto test-w
 
@@ -13,7 +14,9 @@ goto exit
 :run
     node bin/scriptme %2 %3 %4 %5 %6
     goto exit
-
+:test-bin
+  node bin/scriptme -i "./test/cases/jquery.plugin.js" -o "./test/cases/output/myplugin.js" -d "./test/cases/data.json"
+  goto exit
 :test
     ./node_modules/.bin/mocha --reporter %REPORTER%
     goto exit
